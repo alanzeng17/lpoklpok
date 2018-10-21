7760e171716d1f6d9aa3f7a016e81bdd8f78b599
alanzeng17 <alanzeng17@gmail.com>
2018-10-20 19:03:16 -0500

    new stuf

---
+++ frontend/lpoklpok/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomepageLayout from './Homepage.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <HomepageLayout></HomepageLayout>
+    );
+  }
+}
+
+export default App;
